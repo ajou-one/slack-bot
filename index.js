8d688f6ba489a39b2f183c77f51725dfc93a0953
kitez <duswl339@ajou.ac.kr>
2023-03-18 19:29:43 +0900

    feat: add command app.use function

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv/config')
 const schedule = require('node-schedule'); // node schedule
 const { App } = require('@slack/bolt');
 const updateNotice = require("./updateNotice");
+const {noticeAjouUniv, noticeLib, noticeCS, noticeMD, noticeSW, noticeSwCollege, noticeScholar, noticeGG, noticeDom, ajouTeacher} = require("./command/notice");
+const help = require("./command/help");
 
 /**
  * schedule.scheduleJob(scheduling-type, callback)
@@ -26,9 +28,59 @@ const app = new App({
 });
 
 
-// hello 입력받으면 channel에 `Hey there <@${message.user}>!` 출력
-app.message('hello', async ({ message, say }) => {
-    await say(`Hey there <@${message.user}>!`);
+// help
+app.message('help', async ({ message, say }) => {
+    await help({message, say});
+});
+
+// ajou
+app.message('ajou', async ({ message, say }) => {
+    await noticeAjouUniv({message, say});
+});
+
+// college
+app.message('college', async ({ message, say }) => {
+    await noticeSwCollege({message, say});
+});
+
+// sw
+app.message('sw', async ({ message, say }) => {
+    await noticeSW({message, say});
+});
+
+// sc
+app.message('cs', async ({ message, say }) => {
+    await noticeCS({message, say});
+});
+
+// md
+app.message('md', async ({ message, say }) => {
+    await noticeMD({message, say});
+});
+
+// library
+app.message('library', async ({ message, say }) => {
+    await noticeLib({message, say});
+});
+
+// dom
+app.message('dom', async ({ message, say }) => {
+    await noticeDom({message, say});
+});
+
+// teacher
+app.message('teacher', async ({ message, say }) => {
+    await ajouTeacher({message, say});
+});
+
+// gg
+app.message('gg', async ({ message, say }) => {
+    await noticeGG({message, say});
+});
+
+// scholar
+app.message('scholar', async ({ message, say }) => {
+    await noticeScholar({message, say});
 });
 
 function slack() {
@@ -46,5 +98,6 @@ function slack() {
 (async () => {
     await app.start(process.env.PORT || 3000);
     console.log('⚡️ Bolt app is running!');
-    slack();
+    updateNotice();
+    // slack();
 })();
