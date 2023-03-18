7bd4fa5c6a56f6a68d07943dcb555da0cd45d2df
kitez <duswl339@ajou.ac.kr>
2023-03-18 19:30:00 +0900

    feat: modify notice command function

--- command/notice.js
+++ command/notice.js
@@ -1,42 +1,61 @@
-const { WebClient } = require('@slack/web-api');
-const web = new WebClient(process.env.SLACK_BOT_TOKEN);
-
-// 학교 전체
-module.exports = async function noticeAjouUniv() {
-
+// // 학교 전체
+async function noticeAjouUniv({ message, say }) {
+    const response = 'ajou';
+    await say(response);
 }
 
 // 단과대
-module.exports = async function noticeSwCollege() {
-
+async function noticeSwCollege({ message, say }) {
+    const response = 'college';
+    await say(response);
 }
 
 // 소웨
-module.exports = async function noticeSW() {
-
+async function noticeSW({ message, say }) {
+    const response = 'sw';
+    await say(response);
 }
 
 // 사보
-module.exports = async function noticeSC() {
-
+async function noticeCS({ message, say }) {
+    const response = 'cs';
+    await say(response);
 }
 
 // 미디어
-module.exports = async function noticeMD() {
-
+async function noticeMD({ message, say }) {
+    const response = 'md';
+    await say(response);
 }
 
 // 도서관
-module.exports = async function noticeLib() {
-
+async function noticeLib({ message, say }) {
+    const response = 'library';
+    await say(response);
 }
 
 // 경기도
-module.exports = async function noticeGG() {
-
+async function noticeGG({ message, say }) {
+    const response = 'gg';
+    await say(response);
 }
 
 // 한국장학재단
-module.exports = async function noticeScholar() {
-
+async function noticeScholar({ message, say }) {
+    const response = 'scholar';
+    await say(response);
 }
+
+// 기식
+async function noticeDom({ message, say }) {
+    const response = 'dormitory';
+    await say(response);
+}
+
+// 교식
+async function ajouTeacher({ message, say }) {
+    const response = 'teacher';
+    await say(response);
+}
+
+module.exports = {noticeAjouUniv, ajouTeacher, noticeDom, noticeScholar, noticeGG, noticeLib, noticeSW, noticeMD, noticeCS, noticeSwCollege}
