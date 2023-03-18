0b53b39d3fef5c48063cf8bb9d337ef2caa9527c
kitez <duswl339@ajou.ac.kr>
2023-03-18 19:30:24 +0900

    feat: add help command response message

--- command/help.js
+++ command/help.js
@@ -2,8 +2,18 @@ const { WebClient } = require('@slack/web-api');
 const web = new WebClient(process.env.SLACK_BOT_TOKEN);
 
 module.exports = async function help({ message, say }) {
-    // 소개
-    // 사용법
     // 다른 command
-    await say(`help`);
+    const helpMessage = `📌 Command List 📌\n
+- help: print AjouOne Bot command list\n
+- ajou: print Ajou Univ all notifications\n
+- college: print college of Computing and Informatics notifications\n
+- sw: print Software major notifications\n
+- cs: print Cyber Security major notifications\n
+- md: print Media major notifications\n
+- library: print Ajou Library notifications\n
+- dom: print Ajou Dormitary notifications\n
+- food: print Today Ajou restraunt menu\n
+- scholar: print kosaf notifications\n
+- gg: print Gyeonggido notifications`;
+    await say(helpMessage);
 }
